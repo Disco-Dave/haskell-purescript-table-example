65d9725839752cfea3d2ed161e4cee04c5731d4f
David Burkett <david@davidburkett.xyz>
2020-03-05 20:02:57 -0500

    Added dotenv to the server

--- table-example-server/app/Main.hs
+++ table-example-server/app/Main.hs
@@ -4,11 +4,27 @@ import           Relude
 
 import           Api
 
-import qualified Network.Wai.Handler.Warp      as Warp
-import qualified Network.Wai.Middleware.Cors   as Cors
+import qualified Configuration.Dotenv             as Dotenv
+import qualified Configuration.Dotenv.Environment as Env
+import qualified Network.Wai.Handler.Warp         as Warp
+import qualified Network.Wai.Middleware.Cors      as Cors
 
 
 main :: IO ()
-main = 
-  fmap makeApplication makeDbEnv 
-  >>= Warp.run 8081 . Cors.simpleCors
+main = do
+  void loadEnv
+  port <- appPort
+  let 
+    printHost = putTextLn $ "Now listening on http://localhost:" <> show port
+    settings = Warp.setBeforeMainLoop printHost $ Warp.setPort port Warp.defaultSettings
+  app <- Cors.simpleCors . makeApplication <$> makeDbEnv
+  Warp.runSettings settings app
+
+appPort :: IO Int
+appPort = do
+  port <- Env.lookupEnv "SERVER_PORT"
+  pure . fromMaybe 8081 $ port >>= readMaybe
+
+loadEnv :: IO ()
+loadEnv = void $ Dotenv.onMissingFile (Dotenv.loadFile config) (pure [])
+  where config = Dotenv.defaultConfig { Dotenv.configPath = [".env"] }
